178bbbe17ab0d2e143a35a742979bd6239868670
mdpp <mdpp@me.com>
2016-06-26 23:28:00 +0200

    fb 8

--- index.js
+++ index.js
@@ -35,7 +35,7 @@ restService.post('/hook', function (req, res) {
 
 //        console.log('data : {"facebook": {<facebook_message>}}')
 
-
+/*
 > >  	var facebook_message = [ 
 		  {
 > > > >     attachment :{
@@ -70,7 +70,7 @@ restService.post('/hook', function (req, res) {
 > > > >     }
 > > >     }
 		];
-
+*/
 
 		var facebook_message = "";
 
